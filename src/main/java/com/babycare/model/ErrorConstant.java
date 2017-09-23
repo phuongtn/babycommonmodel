7a45bbd8eb35e1ab4c836aa75973c69daa4b7a2f
Phuong Tran <phuong.tran@evizi.com>
2017-09-23 16:01:32 +0700

    update ErrorConstant

--- src/main/java/com/babycare/model/ErrorConstant.java
+++ src/main/java/com/babycare/model/ErrorConstant.java
@@ -1,6 +1,19 @@
 package com.babycare.model;
 
 public class ErrorConstant {
-	public static final Long ERROR_EMAIL_INVALID_CODE = 1L;
+	public static final Long ERROR_EMAIL_INVALID = 1L;
 	public static final String ERROR_EMAIL_INVALID_MESSAGE = "Invalid email";
+
+	public static final Long ERROR_USER_NOT_EXIST = 2L;
+	public static final String ERROR_USER_NOT_EXIST_MESSAGE = "User does not exist";
+	
+	public static final Long ERROR_USER_EXIST = 3L;
+	public static final String ERROR_USER_EXIST_MESSAGE = "User exist";
+
+	public static final Long ERROR_PUSHID_INVALID = 4L;
+	public static final String ERROR_PUSHID_INVALID_MESSAGE = "PushID is not empty";
+	
+	public static final Long ERROR_INPUT_ERROR = 5L;
+	public static final String ERROR_INPUT_ERROR_MESSAGE = "Invalid Data Input";
+
 }

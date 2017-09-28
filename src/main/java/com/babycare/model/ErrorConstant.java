8676eaf5a955b0503706a21c1763e18def933235
Phuong Tran <phuong.tran@evizi.com>
2017-09-28 16:33:09 +0700

    Add constants ERROR_REMOVE_CHILD, ERROR_FETCH_CHILD

--- src/main/java/com/babycare/model/ErrorConstant.java
+++ src/main/java/com/babycare/model/ErrorConstant.java
@@ -21,33 +21,39 @@ public class ErrorConstant {
 	public static final String ERROR_INPUT_ERROR_MESSAGE = "Invalid Data Input";
 
 	public static final Long ERROR_UPDATE_SESSION = 6L;
-	public static final String ERROR_UPDATE_SESSION_MESSAGE = "Failed in updating session";
+	public static final String ERROR_UPDATE_SESSION_MESSAGE = "Failed to update session";
 	
 	public static final Long ERROR_ADD_SESSION = 7L;
-	public static final String ERROR_ADD_SESSION_MESSAGE = "Failed in adding session";
+	public static final String ERROR_ADD_SESSION_MESSAGE = "Failed to add session";
 	
 	public static final Long ERROR_SESSION_NOT_EXIST = 8L;
 	public static final String ERROR_SESSION_NOT_EXIST_MESSAGE = "Session does not exist";
 	
 	public static final Long ERROR_UPDATE_SESSION_STATUS = 9L;
-	public static final String ERROR_UPDATE_SESSION_STATUS_MESSAGE = "Failed in updating session status";
+	public static final String ERROR_UPDATE_SESSION_STATUS_MESSAGE = "Failed to update session status";
 	
 	public static final Long ERROR_CREATE_USER = 10L;
-	public static final String ERROR_CREATE_USER_MESSAGE = "Failed in creating user";
+	public static final String ERROR_CREATE_USER_MESSAGE = "Failed to create user";
 	
 	public static final Long ERROR_UPDATE_SESSION_PUSHID = 11L;
-	public static final String ERROR_UPDATE_SESSION_PUSHID_MESSAGE = "Failed in updating pushId";
+	public static final String ERROR_UPDATE_SESSION_PUSHID_MESSAGE = "Failed in updat pushId";
 	
 	public static final Long ERROR_UPDATE_USER = 12L;
-	public static final String ERROR_UPDATE_USER_MESSAGE = "Failed in updating user";
+	public static final String ERROR_UPDATE_USER_MESSAGE = "Failed to update user";
 	
 	public static final Long ERROR_ADD_CHILD = 13L;
-	public static final String ERROR_ADD_CHILD_MESSAGE = "Failed in adding child";
+	public static final String ERROR_ADD_CHILD_MESSAGE = "Failed to add child";
 	
 	public static final Long ERROR_UPDATE_CHILD = 14L;
-	public static final String ERROR_UPDATE_CHILD_MESSAGE = "Failed in editing child";
+	public static final String ERROR_UPDATE_CHILD_MESSAGE = "Failed to update child";
 	
 	public static final Long ERROR_CHILD_NOT_EXIST = 15L;
 	public static final String ERROR_CHILD_NOT_EXIST_MESSAGE = "Child does not exist";
+	
+	public static final Long ERROR_REMOVE_CHILD = 16L;
+	public static final String ERROR_REMOVE_CHILD_MESSAGE = "Failed to remove child";
+	
+	public static final Long ERROR_FETCH_CHILD = 17L;
+	public static final String ERROR_FETCH_CHILD_MESSAGE = "Failed to fetch child";
 
 }
